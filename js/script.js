948d21ef31ec231a04cf4fcac1dc27cac9a5c4e0
Marcelina Kucięba <marcelinakucieba@wp.pl>
2018-03-04 11:57:57 +0100

    Create site

---
+++ js/script.js
@@ -0,0 +1,39 @@
+function changeBackground(imageURL) {
+    document.body.style.backgroundImage = "url('" + imageURL + "')";
+}
+
+let dataURL = 'https://api.nasa.gov/planetary/apod?api_key=iRRk0rh0x4bHI4UZmAjmhNLaF7N0ArBeQkGvKV1u';
+
+function getPicture() {
+    fetch(dataURL)
+        .then((resp) => {
+            return resp.json();
+        })
+        .then((data) => {
+            changeBackground(data.hdurl);
+        });
+}
+
+let urlMars = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=iRRk0rh0x4bHI4UZmAjmhNLaF7N0ArBeQkGvKV1u";
+
+function getMarsPicture() {
+    fetch(urlMars)
+        .then((resp) => {
+            return resp.json();
+        })
+        .then((data) => {
+            let pictureList = data.photos;
+            createGallery(pictureList);
+        });
+}
+
+let gallery = document.getElementById('content');
+
+function createGallery(dataList) {
+    for(let i = 0; i < 9; i++) {
+        let img = document.createElement('img');
+        let imgPath = dataList[i].img_src;
+        img.src = imgPath;
+        gallery.appendChild(img);
+    }
+}
